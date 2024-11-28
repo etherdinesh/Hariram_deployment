c8b6bebabafe84f668cf140051d40804d91e109c
etherdinesh <etherservices.dinesh@gmail.com>
2024-11-28 15:31:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ // Optional: Include a CSS file for styling import './index.css';
+import App from './App.jsx'; // Your main component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
